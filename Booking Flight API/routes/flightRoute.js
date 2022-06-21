97cee629ce8c6ed4f93f2baad826c5cb4139cf3b
wftID3 <WFT1@IntelliDigest.com>
2022-06-21 16:48:45 +0100

    booking flight api;

---
+++ Booking Flight API/routes/flightRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const controller = require('../controllers/flightController');
+
+const router = express.Router();
+
+// configuring the API routes
+router.get('/', controller.fetch);
+router.get('/:id', controller.fetchid);
+router.post('/add', controller.add);
+router.post('/update/:id', controller.update);
+router.delete('/update/:id', controller.delete);
+
+module.exports = router;

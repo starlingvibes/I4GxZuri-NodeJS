d7d155c534def79a5445e15b166618d5be895cfa
starlingvibes <starlingroot@protonmail.com>
2022-08-05 12:30:06 +0100

    implemented logging in with jwt

--- Authentication and Authorization System/routes/user.js
+++ Authentication and Authorization System/routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/Users');
+require('dotenv').config();
 
 // registration
 router.post('/register', (req, res) => {
@@ -44,6 +46,46 @@ router.delete('/user/delete/:userID', (req, res) => {
     )
     .catch((err) => {
       res.status(500).json({ error: err });
+    });
+});
+
+// logging in
+router.post('/login', (req, res) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((users) => {
+      if (users.length == 0) {
+        return res.sendStatus(404);
+      }
+      bcrypt.compare(req.body.password, users[0].password, (err, same) => {
+        if (err) {
+          res.sendStatus(401);
+        }
+        if (same) {
+          // create a token
+          const token = jwt.sign(
+            {
+              email: users[0].email,
+              userID: users[0]._id,
+            },
+            process.env.SECRET_KEY,
+            {
+              expiresIn: '1h',
+            }
+          );
+          return res.status(200).json({
+            message: 'Authorization successful',
+            token: token,
+          });
+        }
+        res.sendStatus(401);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({
+        error: err,
+      });
     });
 });
 
